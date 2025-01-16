a6bf4a88f98ce867c9ab3ff8ca751a0e4b4f053e
Zhannyhong <33728815+Zhannyhong@users.noreply.github.com>
2025-01-16 17:34:04 +0800

    Refactor greet and bye into functions

--- src/main/java/Gabby.java
+++ src/main/java/Gabby.java
@@ -1,12 +1,19 @@
 public class Gabby {
     private static final String LINE = "____________________________________________________________";
 
-    public static void main(String[] args) {
+    private static void greet() {
         System.out.println(LINE);
         System.out.println("Hello! I'm Gabby\nWhat can I do for you?");
         System.out.println(LINE);
+    }
 
+    private static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE);
+    }
+
+    public static void main(String[] args) {
+        Gabby.greet();
+        Gabby.bye();
     }
 }
