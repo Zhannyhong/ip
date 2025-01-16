4a1f4fd18a00bcd55f592947dc6228938920c1c4
Zhannyhong <33728815+Zhannyhong@users.noreply.github.com>
2025-01-16 17:21:29 +0800

    Rename to Gabby which greets user and exits

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Gabby User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Gabby.java
@@ -0,0 +1,12 @@
+public class Gabby {
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Gabby\nWhat can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
